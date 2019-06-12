b374c45bc1ce0a8b11cc68ca03eb2e13c2d38a89
Alex <sanx46@mail.ru>
2019-06-12 10:32:24 +0400

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+// let answer = +prompt ("Есть ли вам 18?", "Да")
+
+// console.log(typeof(null));
+
+// console.log("arr" + "- object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = true;
+   let  isClose = true;
+
+     console.log(!isChecked || !isClose);
